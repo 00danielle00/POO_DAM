660c1d707806ba60081c6d151d804c9c520a379a
Danielle Bemfica <danielleludovicoo@gmail.com>
2025-02-20 16:06:55 +0100

    commit

---
+++ POO_DAM/src/main/java/dispositivos_inteligentes/AppDispositivos.java
@@ -0,0 +1,45 @@
+package dispositivos_inteligentes;
+
+import java.util.ArrayList;
+
+public class AppDispositivos {
+
+    public static void main(String[] args) {
+
+        Televisor samsung = new Televisor("Samsung");
+        samsung.encender();
+        samsung.encender();
+        samsung.sincronizar();
+
+        ArrayList<Dispositivo> listaDispositivos = new ArrayList<>();
+        listaDispositivos.add(new Televisor("LG SMART"));
+        listaDispositivos.add(new ParlanteInteligente("Alexa"));
+        listaDispositivos.add(new AireAcondicionado("Haier Inverter"));
+        listaDispositivos.add(new Televisor("Samsung comedor"));
+
+        for (Dispositivo dispositivo :listaDispositivos){
+
+            dispositivo.encender();
+
+            if(dispositivo instanceof ControlRemoto){
+                ((ControlRemoto) dispositivo).sincronizar();
+            }
+
+            dispositivo.mostrarEstado();
+            dispositivo.apagar();
+
+            System.out.println("------------------------");
+        }
+
+
+        Dispositivo proyector = new Dispositivo("Proyector") {
+            @Override
+            public void encender() {
+                if (getEstado()){
+
+                }
+            }
+        };
+
+    }
+}
---
+++ POO_DAM/src/main/java/dispositivos_inteligentes/ControlRemoto.java
@@ -0,0 +1,7 @@
+package dispositivos_inteligentes;
+
+public interface ControlRemoto {
+
+    void sincronizar();
+
+}
